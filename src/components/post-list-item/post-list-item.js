b0e2a687db0e87f9dc8ebf87bd1388c434fec4f1
biloholovskyi <biloholovskyi@yandex.ru>
2019-06-10 09:35:05 +0300

    lesson 3 dop task

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './post-list-item.css';
 
 const PostListItem = () => {
+  const time = new Date();
   return (
     <li className="app-list-item d-flex justify-content-between">
       <span className="app-list-item-label">
         Hello World!
       </span>
       <div className="d-flex justify-content-center align-items-center">
+        <span className="list-item-time">{time.toLocaleString([], {day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit'})}</span>
         <button type="button" className="btn-star btn-sm">
           <i className="fa fa-star"></i>
         </button>
